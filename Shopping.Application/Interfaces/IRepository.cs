410b15158bdeab0489800798be7fbd37b93a9c30
kioshos <rkioshos@gmail.com>
2025-03-07 21:54:18 +0200

    Add generic interface for pattern Repository

---
+++ Shopping.Application/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopping.Application.Interfaces;
+
+public interface IRepository<T>
+{
+    List<T> GetAll();
+    
+    T Add(T entity);
+    T Update(T entity);
+    T Delete(int id);
+    T GetById(int id);
+}
